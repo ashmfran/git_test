8e04041ed20a163c36f7cbbd88ba113d2f95bb95
ashmfran <143596924+ashmfran@users.noreply.github.com>
2025-01-27 14:11:58 -0700

    Create myname.py

---
+++ myname.py
@@ -0,0 +1,8 @@
+# myname.py
+name = "Ashley Francis"
+address = "2590 Welton Street Apt 420, Denver CO, 80205"
+telephone_number = "813 892 0899"
+
+print ("Name: ", name)
+print ("Address: ", address)
+print ("Telephone Number: ", telephone_number)
